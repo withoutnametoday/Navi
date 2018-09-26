64e0c89f3778da6b5c7c1069f734ffd3fa44b354
Franco <Franco@LAPTOP-TH8JDV2C>
2018-09-26 00:40:55 -0300

    Original

---
+++ Navi/src/Clases/Usuario.java
@@ -0,0 +1,27 @@
+package Clases;
+
+public class Usuario {
+	private String rut;
+	private String clave;
+	
+	public void setRut( String rt) {
+		rut = rt;
+	}
+	
+	public void setClave( String cl) {
+		clave = cl;
+	}
+	
+	public String getRut() {
+		return rut;
+	}
+	
+	public String getClave() {
+		return clave;
+	}
+	
+	public static void main(String arg[]) {
+		
+		System.out.println("Hola Mundo");
+	}
+}
