145dd18582654f908897d304c05378783f099bd8
Franco <Franco@10.8.234.190>
2018-10-03 12:24:44 -0300

    Ahora?

---
+++ Navi/src/conexion/Usuario.java
@@ -0,0 +1,15 @@
+package conexion;
+
+import java.sql.*;
+import java.sql.Connection;
+
+public class Usuario {
+	
+	private Connection con=null;
+	
+	public Connection conexion() {
+		
+		
+		return con;
+	}
+}
