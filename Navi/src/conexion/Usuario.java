5bbe65f8fed56abd4354d95516109b4c512b04a1
Franco <Franco@10.8.234.190>
2018-10-03 12:59:17 -0300

    Integrando Ventanas

--- Navi/src/conexion/Usuario.java
+++ Navi/src/conexion/Usuario.java
@@ -1,14 +1,24 @@
 package conexion;
 
 import java.sql.*;
-import java.sql.Connection;
+
+import org.graalvm.compiler.hotspot.stubs.StubOptions;
 
 public class Usuario {
 	
-	private Connection con=null;
+	Connection con=null;
 	
 	public Connection conexion() {
-		
+		try {
+			//carla los drivers
+			Class.forName("com.mysql.jdbc.driver");
+			con = DriverManager.getConnection("jdbc:mysql://localHost/user", "root", "");
+			System.out.println("conexion establecida");
+			
+		}
+		catch(ClassNotFoundException | SQLException e){
+			System.out.println("error de conexion" + e);
+		}
 		
 		return con;
 	}
